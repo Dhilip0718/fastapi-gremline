a3b26ffc1335dd00222bd1acdadd921f1d8b09fa
Dhilip RK <dhilipram@gmail.com>
2021-04-08 15:22:16 +0200

    Gremlin change wrt flask

--- application.py
+++ application.py
@@ -1,12 +1,29 @@
-from fastapi import FastAPI
+# Gremlin imports
+from gremlin_python import statics
+from gremlin_python.structure.graph import Graph
+from gremlin_python.process.graph_traversal import __
+from gremlin_python.driver.driver_remote_connection import DriverRemoteConnection
+from flask import Flask
+    
+def invoke_gremlin_connection():
+    graph = Graph()
+    connection = DriverRemoteConnection('ws://10.1.0.4:8182/gremlin', 'g')
+    # The connection should be closed on shut down to close open connections with connection.close()
+    g = graph.traversal().withRemote(connection)
+    # Reuse 'g' across the application
+    gcnt1 = g.V().count()
+    return str(gcnt1)
 
-app = FastAPI()
+app = Flask(__name__)
 
+@app.route("/")
+def hello():
+    return invoke_gremlin_connection()
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.route("/test")
+def test():
+    return 'Check test api url'
 
-@app.get("/items/{item_id}")
-async def read_item(item_id:int):
-    return {"item_id": item_id}
+@app.route("/route")
+def test():
+    return 'Check route api url'
